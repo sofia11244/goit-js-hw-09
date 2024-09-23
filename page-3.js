3b68fdb0e09d413fc9ce8bb3c5c9bf9de1751fe2
sofia11244 <denizsofia03@gmail.com>
2024-09-23 13:57:34 +0000

    Deploying to gh-pages from @ sofia11244/goit-js-hw-09@f210b3f08c7dd8299f620913872f9fed148d175d 🚀

---
+++ page-3.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const a=document.querySelector(".feedback-form");document.querySelector(".feedback-form input");const o="feedback-form-state";let t={email:"",message:""};function s(){const e=localStorage.getItem(o);e&&(t=JSON.parse(e),a.email.value=t.email,a.message.value=t.message)}a.addEventListener("input",e=>{const{name:r,value:m}=e.target;t[r]=m,localStorage.setItem(o,JSON.stringify(t))});a.addEventListener("submit",e=>{e.preventDefault(),console.log(t),localStorage.removeItem(o),a.reset()});s();
+//# sourceMappingURL=page-3.js.map
